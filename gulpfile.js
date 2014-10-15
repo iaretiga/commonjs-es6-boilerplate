9bab23162ea2111331920efe7f2ae8af5cc6b00f
iaretiga <tiga80@gmail.com>
2014-10-15 22:46:30 +0100

    Tweaked gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,58 +8,66 @@ var gulp = require('gulp');
 var jest = require('jest-cli');
 var jstransformVisitors = require('./tools/jstransformVisitors');
 
-var rootDir = 'source';
+var config = {
+    // directory containing your source files
+    sourceRoot: 'source',
 
-var jestConfig = {
-    rootDir: rootDir,
-    scriptPreprocessor: '../tools/jest-preprocessor.js',
+    // output directory / webserver root
+    buildRoot: 'build',
+
+    // main script file, within sourceRoot
+    mainScript: 'app.js'
 };
 
 gulp.task('jshint', function() {
-    gulp.src(rootDir + '/**/*.js')
+    gulp.src(config.sourceRoot + '/**/*.js')
         .pipe(jshint({ esnext: true }))
         .pipe(jshint.reporter(jshintStylish));
 });
 
 gulp.task('build-js', ['jshint'], function() {
-    gulp.src(rootDir + '/app.js')
+    gulp.src(config.sourceRoot + '/' + config.mainScript)
         .pipe(browserify({
             transform: [
                 [ { visitors: jstransformVisitors }, 'jstransformify' ]
             ]
         }))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(config.buildRoot));
 });
 
 gulp.task('build-html', function() {
-    gulp.src(rootDir + '/index.html')
-        .pipe(gulp.dest('./build'));
+    gulp.src(config.sourceRoot + '/index.html')
+        .pipe(gulp.dest(config.buildRoot));
 });
 
 gulp.task('build', ['build-js', 'build-html'], function() {});
 
 gulp.task('watch', function(done) {
 
-    gulp.watch([rootDir + '/**/*.js'], ['build']);
-    gulp.watch([rootDir + '/index.html'], ['build-html']);
+    gulp.watch([config.sourceRoot + '/**/*.js'], ['build']);
+    gulp.watch([config.sourceRoot + '/index.html'], ['build-html']);
 
 });
 
 gulp.task('test', function(done) {
+    var jestConfig = {
+        rootDir: config.sourceRoot,
+        scriptPreprocessor: '../tools/jest-preprocessor.js',
+    };
+
     jest.runCLI({ config : jestConfig }, ".", function() {
         done();
     });
 });
 
 gulp.task('tdd', function(done) {
-    gulp.watch([ jestConfig.rootDir + "/**/*.js" ], [ 'test' ]);
+    gulp.watch([ config.sourceRoot + "/**/*.js" ], [ 'test' ]);
 });
 
 gulp.task('webserver', function() {
-    gulp.src('build')
+    gulp.src(config.buildRoot)
         .pipe(webserver({
-            livereload: true,
-            directoryListing: true
+            livereload: true
         }));
 });
 
