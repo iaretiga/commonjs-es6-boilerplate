d4618c67f8b7feeed841203eeee16d561dd8dd0c
iaretiga <tiga80@gmail.com>
2014-10-18 15:52:38 +0100

    Tweaked gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,14 @@ var config = {
 };
 
 gulp.task('jshint', function() {
+    console.log('Running JsHint on javascript source files.');
     gulp.src(config.sourceRoot + '/**/*.js')
         .pipe(jshint({ esnext: true }))
         .pipe(jshint.reporter(jshintStylish));
 });
 
 gulp.task('build-js', ['jshint'], function() {
+    console.log('Building ' + config.mainScript + '.');
     gulp.src(config.sourceRoot + '/' + config.mainScript)
         .pipe(browserify({
             transform: [
@@ -36,6 +38,7 @@ gulp.task('build-js', ['jshint'], function() {
 });
 
 gulp.task('build-html', function() {
+    console.log('Copying index.html to build folder.');
     gulp.src(config.sourceRoot + '/index.html')
         .pipe(gulp.dest(config.buildRoot));
 });
@@ -43,13 +46,14 @@ gulp.task('build-html', function() {
 gulp.task('build', ['build-js', 'build-html'], function() {});
 
 gulp.task('watch', function(done) {
-
+    console.log('Watching js and html files, rebuilding them on change.');
     gulp.watch([config.sourceRoot + '/**/*.js'], ['build']);
     gulp.watch([config.sourceRoot + '/index.html'], ['build-html']);
 
 });
 
 gulp.task('test', function(done) {
+    console.log('Running unit tests.');
     var jestConfig = {
         rootDir: config.sourceRoot,
         scriptPreprocessor: '../tools/jest-preprocessor.js',
@@ -61,16 +65,16 @@ gulp.task('test', function(done) {
 });
 
 gulp.task('tdd', function(done) {
+    console.log('Watching js source and test files, running unit tests on change.');
     gulp.watch([ config.sourceRoot + "/**/*.js" ], [ 'test' ]);
 });
 
 gulp.task('webserver', function() {
+    console.log('Starting webserver. Serving built application with live reload.');
     gulp.src(config.buildRoot)
         .pipe(webserver({
             livereload: true
         }));
 });
 
-gulp.task('default', function() {
-    // place code for your default task here
-});
+gulp.task('default', ['build'], function() {});
